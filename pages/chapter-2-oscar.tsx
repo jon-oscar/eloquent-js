b986b211ddace2ce19dc4b4b056717b0106036c4
insert-oreyes <oreyes.s@outlook.com>
2023-10-22 19:46:08 +0200

    add creative solution for Pyramid and FizzBuzz

---
+++ pages/chapter-2-oscar.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { CustomButton } from '@/components';
+
+import Pyramid from '@/components/exercises/chapter-2-oscar/Pyramid';
+import FizzBuzz from '@/components/exercises/chapter-2-oscar/fizzBuzz';
+
+const initialState = {
+  rows: 1,
+  phrase: '',
+};
+
+const PageChapter2 = () => {
+  const [state, setState] = useState(initialState);
+
+  return (
+    <div>
+      <div className='Pyramid'>
+        <Pyramid rows={state.rows} />
+        <div>
+          <CustomButton
+            title='Add a row'
+            btnType='button'
+            containerStyles='text-white rounded-full bg-[#B2980B] min-w-[150px]'
+            handleClick={() =>
+              setState((prevState) => ({
+                ...prevState,
+                rows: prevState.rows + 1,
+              }))
+            }
+          />
+        </div>
+        <div>
+          <CustomButton
+            title='Remove a row'
+            btnType='button'
+            containerStyles='text-white rounded-full bg-[#B2980B] min-w-[150px]'
+            handleClick={() =>
+              setState((prevState) => ({
+                ...prevState,
+                rows: prevState.rows - 1,
+              }))
+            }
+          />
+        </div>
+      </div>
+
+      <div className='FizzBuzz'>
+        <FizzBuzz
+          phraseValue={state.phrase}
+          onChange={(e) =>
+            setState((prevState) => ({
+              ...prevState,
+              phrase: e.target.value,
+            }))
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PageChapter2;
