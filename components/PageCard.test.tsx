caa4694a997de2736e54be9bdc3b7df9a64daef8
insert-oreyes <oreyes.s@outlook.com>
2023-10-24 17:30:17 +0200

    Bug testing was not working, delete package-lock and node_modules and reinstall

--- components/PageCard.test.tsx
+++ components/PageCard.test.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import PageCard, { Info } from './PageCard';
 
 describe('PageCard', () => {
-    const cardInfo: Info[] = [
-        {
-            id: 1,
-            title: 'Title 1',
-            details: 'Details 1',
-            code: (name: string) => console.log(`Hello, ${name}!`),
-        },
-        {
-            id: 2,
-            title: 'Title 2',
-            details: 'Details 2',
-            code: (name: string) => console.log(`Goodbye, ${name}!`),
-        },
-    ];
+  const cardInfo: Info[] = [
+    {
+      id: 1,
+      title: 'Title 1',
+      details: 'Details 1',
+      code: (name: string) => console.log(`Code for ${name}`),
+    },
+    {
+      id: 2,
+      title: 'Title 2',
+      details: 'Details 2',
+      code: (name: string) => console.log(`Code for ${name}`),
+    },
+  ];
 
-    it('renders a card for each item in cardInfo', () => {
-        const { getAllByTestId } = render(<PageCard cardInfo={cardInfo} />);
-        expect(getAllByTestId('page-card')).toHaveLength(cardInfo.length);
-    });
+  it('renders the correct number of cards', () => {
+    render(<PageCard cardInfo={cardInfo} />);
+    expect(screen.getAllByTestId('page-card')).toHaveLength(cardInfo.length);
+  });
 
-    it('renders the title and details for each card', () => {
-        const { getByText } = render(<PageCard cardInfo={cardInfo} />);
-        cardInfo.forEach(({ title, details }) => {
-            expect(getByText(title)).toBeInTheDocument();
-            expect(getByText(details)).toBeInTheDocument();
-        });
+  it('renders the correct title and details for each card', () => {
+    render(<PageCard cardInfo={cardInfo} />);
+    cardInfo.forEach((info) => {
+      expect(screen.getByText(info.title)).toBeInTheDocument();
+      expect(screen.getByText(info.details)).toBeInTheDocument();
     });
+  });
 });
