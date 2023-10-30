347a09542cade9984f545705f478974092ccc872
insert-oreyes <oreyes.s@outlook.com>
2023-10-30 08:09:18 +0100

    update files and branch bug solved

--- pages/chapter2.tsx
+++ pages/chapter2.tsx
@@ -3,6 +3,10 @@ import PageCard from '@/components/PageCard';
 import Layout from '@/components/Layout';
 import chapter from '@/constants/chapter.json';
 
+import Pyramid from '@/components/exercises/chapter-2-oscar/Pyramid';
+import FizzBuzz from '@/components/exercises/chapter-2-oscar/FizzBuzz';
+import Chessboard from '@/components/exercises/chapter-2-oscar/Chessboard';
+
 const chapterId = 2;
 
 const chapterMatch = chapter.find((chapter) => chapter.id === chapterId);
@@ -27,10 +31,38 @@ export default function Chapter2() {
 
         <div className='flex xl:flex-row flex-col gap-5 z-0 relative max-w-[1440px] mx-auto'>
           <div className='flex flex-col'>
-            <PageCard cardInfo={devOscar as []} />
+            {devOscar && (
+              <>
+                <PageCard
+                  id={devOscar[0].id}
+                  title={devOscar[0].title}
+                  details={devOscar[0].details}
+                  code={() => <Pyramid rows={0} />}
+                />
+                <PageCard
+                  id={devOscar[1].id}
+                  title={devOscar[1].title}
+                  details={devOscar[1].details}
+                  code={() => <FizzBuzz phraseValue={''} />}
+                />
+                <PageCard
+                  id={devOscar[2].id}
+                  title={devOscar[2].title}
+                  details={devOscar[2].details}
+                  code={() => <Chessboard size={8} />}
+                />
+              </>
+            )}
           </div>
           <div className='flex flex-col'>
-            <PageCard cardInfo={devJon as []} />
+            {devJon && (
+              <PageCard
+                id={devJon[0].id}
+                title={devJon[0].title}
+                details={devJon[0].details}
+                code={() => 'YOUR CODE GOES HERE'}
+              />
+            )}
           </div>
         </div>
       </div>
