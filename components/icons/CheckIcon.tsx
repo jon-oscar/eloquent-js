09452961e96678b4679fa8bc3c95f634fd20a050
Jon Portella <jportella93@gmail.com>
2023-11-07 09:01:57 -0500

    Add CheckIcon

---
+++ components/icons/CheckIcon.tsx
@@ -0,0 +1,17 @@
+/**
+ * CheckIcon component displays a checkmark icon.
+ */
+export default function CheckIcon(props: any) {
+  return (
+    <svg viewBox='0 0 24 24' fill='none' data-testid='check-icon' {...props}>
+      <circle cx={12} cy={12} r={12} fill='#fff' opacity='0.2' />
+      <path
+        d='M7 13l3 3 7-7'
+        stroke='#fff'
+        strokeWidth={1.5}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
