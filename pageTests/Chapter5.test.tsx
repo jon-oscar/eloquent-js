7d4acaaa2479820cd6c46e48772ffa8136bc5b23
insert-oreyes <oreyes.s@outlook.com>
2023-12-01 12:13:49 +0100

    create and update test for Chapter5

---
+++ pageTests/Chapter5.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import Chapter5 from '../pages/chapter5';
+
+describe('Chapter5', () => {
+  it('renders the title, subtitle and details', () => {
+    render(<Chapter5 />);
+    const title = screen.getByText(/Chapter 5/i);
+    const subtitle = screen.getByText('Flattering');
+    const details = screen.getByText((details) =>
+      details.startsWith(
+        'Being able to pass function values to other functions is a deeply useful aspect of JavaScript.'
+      )
+    );
+    expect(title).toBeInTheDocument();
+    expect(subtitle).toBeInTheDocument();
+    expect(details).toBeInTheDocument();
+  });
+
+  it('renders the flattering exercise', () => {
+    render(<Chapter5 />);
+    const flattering = screen.getByText(/Flattering/i);
+    expect(flattering).toBeInTheDocument();
+  });
+
+  it('renders the developer card for Oscar Reyes', () => {
+    render(<Chapter5 />);
+    const oscarCard = screen.getByText(/Oscar Reyes/i);
+    expect(oscarCard).toBeInTheDocument();
+  });
+});
