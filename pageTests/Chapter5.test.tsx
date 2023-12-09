6fa22eae1f5a8e5ca552d2303a8304dd83e41a6c
insert-oreyes <oreyes.s@outlook.com>
2023-12-09 22:51:54 +0100

    update some missing data from main origin merge

--- pageTests/Chapter5.test.tsx
+++ pageTests/Chapter5.test.tsx
@@ -22,34 +22,10 @@ describe('Chapter5', () => {
     expect(flattening).toBeInTheDocument();
   });
 
-  it('renders the developer card for Oscar Reyes', () => {
-    render(<Chapter5 />);
-    const oscarCard = screen.getByText(/Oscar Reyes/i);
-    expect(oscarCard).toBeInTheDocument();
-  });
-});
-import { render, screen } from '@testing-library/react';
-import Chapter5 from '../pages/chapter5';
-
-describe('Chapter5', () => {
-  it('renders the title, subtitle and details', () => {
-    render(<Chapter5 />);
-    const title = screen.getByText(/Chapter 5/i);
-    const subtitle = screen.getByText('Your own loop');
-    const details = screen.getByText((details) =>
-      details.startsWith(
-        'Being able to pass function values to other functions is a deeply useful aspect of JavaScript.'
-      )
-    );
-    expect(title).toBeInTheDocument();
-    expect(subtitle).toBeInTheDocument();
-    expect(details).toBeInTheDocument();
-  });
-
   it('renders the flattening exercise', () => {
     render(<Chapter5 />);
-    const flattening = screen.getByText(/YourOwnLoop/i);
-    expect(flattening).toBeInTheDocument();
+    const yourOwnLoop = screen.getByText(/Your Own Loop/i);
+    expect(yourOwnLoop).toBeInTheDocument();
   });
 
   it('renders the developer card for Oscar Reyes', () => {
