6a7a0950d002897b3d69fc799d9d8af37de28302
insert-oreyes <oreyes.s@outlook.com>
2024-02-02 13:57:47 +0100

    exercise working and tests passed

--- components/exercises/chapter-6-oscar/VectorType/Vec.tsx
+++ components/exercises/chapter-6-oscar/VectorType/Vec.tsx
@@ -1,17 +1,39 @@
+/**
+ * Represents a 2D vector.
+ */
 export class Vec {
+  /**
+   * Creates a new vector.
+   * @param x The x-coordinate of the vector.
+   * @param y The y-coordinate of the vector.
+   */
   constructor(
     public x: number,
     public y: number
   ) {}
 
+  /**
+   * Adds another vector to this vector.
+   * @param other The vector to be added.
+   * @returns The sum of the two vectors.
+   */
   plus(other: Vec) {
     return new Vec(this.x + other.x, this.y + other.y);
   }
 
+  /**
+   * Subtracts another vector from this vector.
+   * @param other The vector to be subtracted.
+   * @returns The difference between the two vectors.
+   */
   minus(other: Vec) {
     return new Vec(this.x - other.x, this.y - other.y);
   }
 
+  /**
+   * Gets the length of the vector.
+   * @returns The length of the vector.
+   */
   get length() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
--- components/exercises/chapter-6-oscar/VectorType/VectorType.tsx
+++ components/exercises/chapter-6-oscar/VectorType/VectorType.tsx
@@ -1,52 +1,83 @@
 import React, { useState } from 'react';
 import { Vec } from './Vec'; // Import the Vec class
 
-function VectorType() {
+/**
+ * Represents a square and provides controls to increase or decrease the size.
+ */
+export default function VectorType() {
   const [vector, setVector] = useState(new Vec(60, 60)); // Initial vector with width and height
 
-  const handleWidthChange = (event) => {
+  // Event handler for changing the width input
+  const handleWidthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newWidth = Number(event.target.value);
     setVector((prevVector) => new Vec(newWidth, prevVector.y));
   };
 
-  const handleHeightChange = (event) => {
+  // Event handler for changing the height input
+  const handleHeightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newHeight = Number(event.target.value);
     setVector((prevVector) => new Vec(prevVector.x, newHeight));
   };
 
+  // Event handler for increasing the vector by 10 in both width and height
   const handlePlus = () => {
     setVector((prevVector) => prevVector.plus(new Vec(10, 10)));
   };
 
+  // Event handler for decreasing the vector by 10 in both width and height
   const handleMinus = () => {
     setVector((prevVector) => prevVector.minus(new Vec(10, 10)));
   };
 
+  // Event handler for displaying an alert with the length of the vector
   const handleLength = () => {
     alert(`Length: ${vector.length}`);
   };
 
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center'>
       <div
         className={`w-${vector.x} h-${vector.y} bg-blue-500`}
         style={{ width: `${vector.x}px`, height: `${vector.y}px` }}
       ></div>
       <div>
-        <label>Width:</label>
-        <input type='number' value={vector.x} onChange={handleWidthChange} />
+        <label htmlFor='widthInput'>Width:</label>
+        <input
+          id='widthInput'
+          onChange={handleWidthChange}
+          type='number'
+          value={vector.x}
+        />
       </div>
       <div>
-        <label>Height:</label>
-        <input type='number' value={vector.y} onChange={handleHeightChange} />
+        <label htmlFor='heightInput'>Height:</label>
+        <input
+          id='heightInput'
+          onChange={handleHeightChange}
+          type='number'
+          value={vector.y}
+        />
       </div>
-      <div>
-        <button onClick={handlePlus}>Increase by 10</button>
-        <button onClick={handleMinus}>Decrease by 10</button>
-        <button onClick={handleLength}>Get Length</button>
+      <div className='mt-4 space-x-4'>
+        <button
+          className='rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600'
+          onClick={handlePlus}
+        >
+          Increase by 10
+        </button>
+        <button
+          className='rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600'
+          onClick={handleMinus}
+        >
+          Decrease by 10
+        </button>
+        <button
+          className='rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600'
+          onClick={handleLength}
+        >
+          Get Length
+        </button>
       </div>
     </div>
   );
 }
-
-export default VectorType;
