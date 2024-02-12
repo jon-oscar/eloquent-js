977965b183018a18eb0a356eeef38121b2ef2e93
insert-oreyes <oreyes.s@outlook.com>
2024-02-12 09:29:06 +0100

    modify exercise 2 including exercise 3 requirements

--- components/exercises/chapter-6-oscar/group/Group.test.tsx
+++ components/exercises/chapter-6-oscar/group/Group.test.tsx
@@ -1,4 +1,5 @@
 import Group from './Group';
+import GroupIterator from './Group';
 
 describe('Group', () => {
   let group: Group<string>;
@@ -30,5 +31,28 @@ describe('Group', () => {
     const members = ['Oscar', 'Daniel', 'Frank'];
     const groupFromMembers = Group.from(members);
     expect(groupFromMembers.getMembers()).toEqual(members);
+  });
+
+  it('should iterate over the group using Symbol.iterator', () => {
+    group.add('Oscar');
+    group.add('Daniel');
+    const iterator = group[Symbol.iterator]();
+    expect(iterator.next().value).toBe('Oscar');
+    expect(iterator.next().value).toBe('Daniel');
+    expect(iterator.next().done).toBe(true);
+  });
+
+  it('should iterate over members correctly', () => {
+    const group = new Group<string>();
+    group.add('Oscar');
+    group.add('Daniel');
+    group.add('Frank');
+
+    const iterator = group[Symbol.iterator]();
+
+    expect(iterator.next()).toEqual({ value: 'Oscar', done: false });
+    expect(iterator.next()).toEqual({ value: 'Daniel', done: false });
+    expect(iterator.next()).toEqual({ value: 'Frank', done: false });
+    expect(iterator.next()).toEqual({ value: undefined, done: true });
   });
 });
