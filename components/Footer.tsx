3a96287c8134417cae3ebbb1e2b58007c1bf33e2
Oscar R <99765175+insert-oreyes@users.noreply.github.com>
2024-03-05 21:03:16 +0100

    Remove unused links in footer (#63)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,4 @@
-import footerLinks from '@/constants/footer.json';
 import Image from 'next/image';
-import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -16,40 +14,8 @@ const Footer = () => {
           ></Image>
           <p className='text-base text-gray-700'>
             Project by Oscar Reyes <br />
-            All rights reserved
+            mentored by Jon Portella
           </p>
-        </div>
-
-        <div className='footer__links'>
-          {footerLinks.map((item) => (
-            <div className='footer__link' key={item.title}>
-              <h3 className='font-bold'>{item.title}</h3>
-              <div className='flex flex-col gap-5'>
-                {item.links.map((link) => (
-                  <Link
-                    className='text-gray-500'
-                    href={link.url}
-                    key={link.title}
-                  >
-                    {link.title}
-                  </Link>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className='mt-10 flex flex-wrap items-center justify-between border-t border-gray-100 px-6 py-10 sm:px-16'>
-        <p>oreyesdev. All rights reserved</p>
-
-        <div className='footer__copyrights-link'>
-          <Link className='text-gray-500' href='/'>
-            Privacy & Policy
-          </Link>
-          <Link className='text-gray-500' href='/'>
-            Terms & Condition
-          </Link>
         </div>
       </div>
     </footer>
